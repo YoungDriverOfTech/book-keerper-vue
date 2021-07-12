d7de798821523003b99d2b84c97720d8290d6f7d
liang <xiaoleliang045@gmail.com>
2021-07-12 11:18:47 +0900

    my first component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Liang></Liang>
+</template>
+
+<script>
+import Liang from './components/Liang.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Liang
+  }
+}
+</script>
