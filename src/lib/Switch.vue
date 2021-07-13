1634e24f1bfa145740ed92bde96342314e754ea1
liang <xiaoleliang045@gmail.com>
2021-07-13 23:37:44 +0900

    switch move

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,9 +1,20 @@
 <template>
-    <button><span></span></button>
+    <button @click="toggle" :class="{checked}"><span></span></button>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
+
 export default {
+    setup() {
+        // this flag is used to controll span's move
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value;
+        }
+
+        return {checked, toggle}
+    }
 
 }
 </script>
@@ -15,7 +26,7 @@ export default {
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    background: gray;
     border-radius: $h/2;
     position: relative;
   }
@@ -30,7 +41,11 @@ export default {
     border-radius: $h2 / 2;
   }
 
-  button:hover > span{
+  button.checked{
+    background: blue;
+  }
+
+  button.checked > span{
       left: calc(100% - #{$h2} - 2px);
   }
 </style>
