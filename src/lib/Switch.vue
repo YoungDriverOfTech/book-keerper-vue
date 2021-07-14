3df76f9961d36233ead3c779b1422459f8d9e397
liang <xiaoleliang045@gmail.com>
2021-07-14 22:43:39 +0900

    child emit event change parent value

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,19 +1,22 @@
 <template>
-    <button @click="toggle" :class="{checked}"><span></span></button>
+    <button @click="toggle" :class="{checked: value}"><span></span></button>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue'
 
 export default {
-    setup() {
+    props: {
+      value: Boolean
+    },
+
+    setup(props, context) {
         // this flag is used to controll span's move
-        const checked = ref(false);
         const toggle = () => {
-            checked.value = !checked.value;
+          context.emit('input', !props.value)
         }
 
-        return {checked, toggle}
+        return {toggle}
     }
 
 }
@@ -39,6 +42,8 @@ export default {
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
+    // constrain change happens within given period
+    transition: left 250ms;
   }
 
   button.checked{
@@ -46,6 +51,10 @@ export default {
   }
 
   button.checked > span{
-      left: calc(100% - #{$h2} - 2px);
+    left: calc(100% - #{$h2} - 2px);
+  }
+
+  button:focus {
+    outline: none;
   }
 </style>
