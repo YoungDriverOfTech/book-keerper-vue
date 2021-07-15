1c8870fb2cb90cb79799a4c48a824839df9b1f8b
liang <xiaoleliang045@gmail.com>
2021-07-15 17:32:59 +0900

    css modification

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,16 +2,27 @@
     <div>
         <h1>示例1</h1>
         <br/>
-        <Button theme="button">hello</Button>
-        <Button theme="link">hello</Button>
-        <Button theme="text">hello</Button>
+        <Button theme="button">button</Button>
+        <Button theme="link">link</Button>
+        <Button theme="text">text</Button>
     </div>
     <div>
+        <br/>
         <h1>示例2</h1>
         <br/>
-        <Button size="small">hello</Button>
-        <Button size="normal">hello</Button>
-        <Button size="big">hello</Button>
+        <Button theme="button" size="small">small button</Button>
+        <Button theme="button" size="normal">normal button</Button>
+        <Button theme="button" size="big">big button</Button>
+    </div>
+    <div>
+        <Button theme="link" size="small">small button</Button>
+        <Button theme="link" size="normal">normal button</Button>
+        <Button theme="link" size="big">big button</Button>
+    </div>
+    <div>
+        <Button theme="text" size="small">small button</Button>
+        <Button theme="text" size="normal">normal button</Button>
+        <Button theme="text" size="big">big button</Button>
     </div>
 </template>
 
