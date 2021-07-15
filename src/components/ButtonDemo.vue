cb252c272d60ceb0008fe1d0a41840cde0da6540
liang <xiaoleliang045@gmail.com>
2021-07-15 20:51:09 +0900

    level properties

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -15,15 +15,33 @@
         <Button theme="button" size="big">big button</Button>
     </div>
     <div>
-        <Button theme="link" size="small">small button</Button>
-        <Button theme="link" size="normal">normal button</Button>
-        <Button theme="link" size="big">big button</Button>
+        <Button theme="link" size="small">small link</Button>
+        <Button theme="link" size="normal">normal link</Button>
+        <Button theme="link" size="big">big link</Button>
     </div>
     <div>
-        <Button theme="text" size="small">small button</Button>
-        <Button theme="text" size="normal">normal button</Button>
-        <Button theme="text" size="big">big button</Button>
+        <Button theme="text" size="small">small text</Button>
+        <Button theme="text" size="normal">normal text</Button>
+        <Button theme="text" size="big">big text</Button>
     </div>
+     <div>
+        <br/>
+        <h1>示例3</h1>
+        <br/>
+        <Button theme="button" level="main">main button</Button>
+        <Button theme="button" level="normal">normal button</Button>
+        <Button theme="button" level="danger">danger button</Button>
+    </div>
+    <div>
+        <Button theme="link" level="main">main link</Button>
+        <Button theme="link" level="normal">normal link</Button>
+        <Button theme="link" level="danger">danger link</Button>
+    </div>
+    <div>
+        <Button theme="text" level="main">main text</Button>
+        <Button theme="text" level="normal">normal text</Button>
+        <Button theme="text" level="danger">danger text</Button>
+    </div>   
 </template>
 
 <script lang="ts">
