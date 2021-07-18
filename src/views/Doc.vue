6b63d96333939b4f751db92b4eb1d18fa6051f8a
liang <xiaoleliang045@gmail.com>
2021-07-18 14:48:01 +0900

    document update

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -85,7 +85,6 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
   position: fixed;
   top: 0;
   left: 0;
@@ -93,10 +92,18 @@ aside {
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol {
     > li {
-      padding: 4px 0;
+      >a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
 }
